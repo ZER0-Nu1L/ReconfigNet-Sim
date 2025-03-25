728b163daf1009cafc1195c4bde3ce9a78555bb6
ZER0-Nu1L <wcb534813346@126.com>
2025-03-25 20:34:39 +0800

    [feat] P4 implements ocs w/ two p4app version

---
+++ ocs.p4app/ocs.p4app-rc2/include/header.p4
@@ -0,0 +1,55 @@
+#ifndef __HEADER_P4__
+#define __HEADER_P4__ 1
+
+struct ingress_metadata_t {
+    bit<32> nhop_ipv4;
+}
+
+header ethernet_t {
+    bit<48> dstAddr;
+    bit<48> srcAddr;
+    bit<16> etherType;
+}
+
+header ipv4_t {
+    bit<4>  version;
+    bit<4>  ihl;
+    bit<8>  diffserv;
+    bit<16> totalLen;
+    bit<16> identification;
+    bit<3>  flags;
+    bit<13> fragOffset;
+    bit<8>  ttl;
+    bit<8>  protocol;
+    bit<16> hdrChecksum;
+    bit<32> srcAddr;
+    bit<32> dstAddr;
+}
+
+header arp_t {
+    bit<16> htype;
+    bit<16> ptype;
+    bit<8>  hlen;
+    bit<8>  plen;
+    bit<16> oper;
+    bit<48> sha;
+    bit<32> spa;
+    bit<48> tha;
+    bit<32> tpa;
+}
+
+struct metadata {
+    @name("ingress_metadata")
+    ingress_metadata_t   ingress_metadata;
+}
+
+struct headers {
+    @name("ethernet")
+    ethernet_t ethernet;
+    @name("ipv4")
+    ipv4_t     ipv4;
+    @name("arp")
+    arp_t      arp;
+}
+
+#endif // __HEADER_P4__
---
+++ ocs.p4app/ocs.p4app-rc2/main.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+from p4app import P4Mininet
+from mininet.topo import Topo
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+import struct, os
+
+def hostIP(i, mask=False):
+    '''
+    l3 mode
+    '''
+    if mask == True:
+        return "10.0.%d.10/24" % (i)
+    else:
+        return "10.0.%d.10" % (i)
+    # l2 mode
+    # if mask == True:
+    #     return "10.0.10.%d/24" % (i)
+    # else:
+    #     return "10.0.10.%d" % (i)
+
+def hostMAC(i):
+    return '00:00:00:00:00:%02x' % (i)
+
+def switchIP(i):
+    return "10.0.%d.1" % (i)
+
+def switchMAC(i):
+    return '00:aa:bb:00:00:%02x' % (i)
+
+class CustomTopo(Topo):
+    def __init__(self, num_hosts, mode, **opts):
+        Topo.__init__(self, **opts)
+        switch = self.addSwitch('s1')
+
+        for i in range(1, num_hosts+1):
+            host = self.addHost('h%d' % i, ip = hostIP(i, mask=True), mac = hostMAC(i))
+            self.addLink(host, switch, port2=i) # port2: dest port
+            # NOTE: Only responsible for L2 connectivity
+            # gateway, etc. does not seem to be set by default
+
+def main():
+    setLogLevel('info')
+    
+    mode = 'l3'
+    num_hosts = 8
+    enable_debugger = False
+
+    topo = CustomTopo(num_hosts, mode)
+    net = P4Mininet(program='ocs.p4', topo=topo, enable_debugger=enable_debugger)
+    # NOTE: set setup every host ARP to every host by default.
+    net.start()
+
+    '''
+    Hosts states setup
+    '''
+    for i in range(1, num_hosts+1):
+        h = net.get('h%d' % (i))
+        if mode == "l2":
+            h.setDefaultRoute("dev %s" % h.defaultIntf().name)
+        elif mode == "l3":
+            h.setARP(switchIP(i), switchMAC(i))
+            h.setDefaultRoute("dev %s via %s" % (h.defaultIntf().name, switchIP(i)))
+        else:
+            assert mode != 'l2' and mode != 'l3'
+            exit()
+
+    '''
+    Switch states (table entries) setup
+    '''
+    tb_forward_entries = []
+    tb_ipv4_lpm_entries = []
+    # tb_arp_forward_entries = []
+    tb_send_frame_entries = []
+    tb_ocs_mapping_entries = [] # TODO: 
+    all_table_entries = [
+        tb_ipv4_lpm_entries, 
+        # tb_arp_forward_entries, 
+        tb_forward_entries,
+        tb_send_frame_entries,
+        tb_ocs_mapping_entries # TODO: 
+    ]
+    for i in range(1, num_hosts+1):
+        port = i
+        
+        tb_ipv4_lpm_entries.append(dict(
+                table_name      = 'ingress.ipv4_lpm',
+                match_fields    = {'hdr.ipv4.dstAddr': [hostIP(i), 32]}, 
+                action_name     = 'ingress.set_nhop',
+                action_params   = {'nhop_ipv4': hostIP(i), 'port': port}
+                ))
+                # NOTE: L3 forward table
+
+        # tb_arp_forward_entries.append(dict(
+        #         table_name      = 'ingress.arp_forward',
+        #         match_fields    = {'hdr.arp.tpa': [hostIP(i)]},
+        #         action_name     = 'ingress.set_egress_port',
+        #         action_params   = {'port': port}
+        #         ))
+
+        tb_forward_entries.append(dict(
+                table_name      = 'ingress.forward',
+                match_fields    = {'meta.ingress_metadata.nhop_ipv4': [hostIP(i)]},
+                action_name     = 'ingress.set_dmac',
+                action_params   = {'dmac': hostMAC(i)}
+                ))
+                # NOTE: This is the ARP table of switch as an L3 device 
+                # bonding the protocol layers of L2 and L3 (L3 -> L2)
+
+
+        tb_send_frame_entries.append(dict(
+                table_name      = 'egress.send_frame',
+                match_fields    = {'standard_metadata.egress_port': [port]},
+                action_name     = 'egress.rewrite_mac',
+                action_params   = {'smac' : switchMAC(i)}
+                ))
+                # NOTE: L2 Logic
+
+
+        if num_hosts >= 2:
+            for i in range(1, num_hosts, 2):
+                tb_ocs_mapping_entries.append(dict(
+                        table_name    = 'egress.ocs_mapping',
+                        match_fields  = {'standard_metadata.ingress_port': [(i)], 'standard_metadata.egress_port': [(i+1)]},
+                        action_name   = 'NoAction',
+                        action_params = {}
+                        ))
+                tb_ocs_mapping_entries.append(dict(
+                        table_name    = 'egress.ocs_mapping',
+                        match_fields  = {'standard_metadata.ingress_port': [(i+1)], 'standard_metadata.egress_port': [(i)]},
+                        action_name   = 'NoAction',
+                        action_params = {}))
+
+    s1 = net.get('s1')
+    for table_entries in all_table_entries:
+        for table_entry in table_entries:
+            s1.insertTableEntry(table_entry)
+    
+    info("***** Installing default table entries on switch s1 *****\n")
+    s1.printTableEntries()
+    info("***** All table entries installed. Network is ready! *****\n")
+    
+    # NOTE: for Debug in container
+    if(enable_debugger):
+        container = os.environ['HOSTNAME']
+        s1_logfile = "/tmp/p4app-logs/p4s.{}.log".format(s1.name)
+        print('---------------------------------------------------------')
+        print('CLI from host operating system using this command:')
+        print('  docker exec -t -i %s simple_switch_CLI\n' % container)
+        print('To view the switch %s log, run this command from your host OS:' % s1.name)
+        print('  docker exec -t -i %s tail -f %s\n' % (container, s1_logfile))
+        print('To run the switch debugger, run this command from your host OS:')
+        print('  docker exec -t -i %s bm_p4dbg\n' % container)
+        print('---------------------------------------------------------')    
+
+    loss = net.pingAll(timeout = 1)
+    if(enable_debugger):
+        CLI(net)
+    else:
+        # assert loss == 0
+        pass
+    
+    net.stop()
+
+if __name__ == '__main__':
+    main()
